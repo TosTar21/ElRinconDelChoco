761275eea67b4b8a74b6737238f9730646eea5e2
Tostar21 <oscar.zuniga.sanchez@est.una.ac.cr>
2025-05-04 11:04:15 -0600

    I added the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,31 @@
 import Navbar from './components/layout/Navbar';
-import Carousel from './components/microcomponents/Carousel';
 import Modal from './components/microcomponents/Modal';
 import { useAppLogic } from './hooks/useAppLogic';
+import HeroSection from './components/sections/HeroSection';
+import MovieSection from './components/sections/MovieSection';
+import BookSection from './components/sections/BookSection';
 
 const App = () => {
   const {
-    mode,setMode,showPreferences,openPreferences,showModal,openModal,
-    closeModal,selectedItem,movies,loadingMovies,books,loadingBooks,
+    mode, setMode, showPreferences, openPreferences,
+    showModal, openModal, closeModal, selectedItem,
+    movies, loadingMovies, books, loadingBooks,
   } = useAppLogic();
 
   return (
     <div className="bg-gray-900 min-h-screen text-white">
       <Navbar mode={mode} setMode={setMode} openPreferences={openPreferences} />
+      
+      <HeroSection />
 
       <div className="p-4 space-y-10">
-
         {mode === 'movies' && !loadingMovies && movies && (
-          <>
-            <section>
-              <h2 className="text-2xl font-bold mb-4">Más vistas</h2>
-              <Carousel items={movies} type="movies" onSelect={openModal} />
-            </section>
-
-            <section>
-              <h2 className="text-2xl font-bold mb-4">Recomendadas</h2>
-              <Carousel items={movies} type="movies" onSelect={openModal} />
-            </section>
-          </>
+          <MovieSection movies={movies} openModal={openModal} />
         )}
 
         {mode === 'books' && !loadingBooks && books && (
-          <>
-            <section>
-              <h2 className="text-2xl font-bold mb-4">Más vistas</h2>
-              <Carousel items={books} type="books" onSelect={openModal} />
-            </section>
-
-            <section>
-              <h2 className="text-2xl font-bold mb-4">Recomendadas</h2>
-              <Carousel items={books} type="books" onSelect={openModal} />
-            </section>
-          </>
+          <BookSection books={books} openModal={openModal} />
         )}
-
       </div>
 
       <Modal isOpen={showModal} closeModal={closeModal} item={selectedItem} type={mode} />
